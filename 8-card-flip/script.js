f29aac1bc52a2b7146dc99256019ba50d79a4221
Linelinlove <chrisline.lin@outlook.fr>
2023-08-25 23:31:47 +0200

    small fix

--- 8-card-flip/script.js
+++ 8-card-flip/script.js
@@ -1,5 +1,14 @@
+// for single card
 const card = document.querySelector('.card__inner');
 
 card.addEventListener('click', function () {
     card.classList.toggle('is-flipped');
 })
+
+
+// for multiple cards
+// const cards = document.querySelectorAll('.card__inner');
+
+// cards.forEach((cards) => cards.addEventListener('click', function () {
+//     cards.classList.toggle('is-flipped');
+// }));
